83c167d4a90c36b7f76bb4318e935b39304c42a7
Marcos Ribeiro <marcosribeiro15@gmail.com>
2016-12-09 02:20:39 -0300

    atualizacao

--- php/Controle/mapping.php
+++ php/Controle/mapping.php
@@ -32,15 +32,14 @@ class MappingControle{
 		if(empty($_SESSION['log'])) $_SESSION['log'] = 'Mapeamento adicionado com sucesso';
 	}
 	public function addmappingLB(){
-		// if(!isset($_SESSION['serverODL']) || empty($_SESSION['serverODL'])) header('location: ../../index.php');
-		// else $serverODL=$_SESSION['serverODL'];
+		if(!isset($_SESSION['serverODL']) || empty($_SESSION['serverODL'])) header('location: ../../index.php');
+		else $serverODL=$_SESSION['serverODL'];
 
-		// $serverODL='10.132.12.138:8181';
-		// MappingControle::make_addmapping_lb();
-		// $script='curl -u "admin":"admin" -H "Content-type: application/json" -X POST http://'.$serverODL.'/restconf/operations/odl-mappingservice:add-mapping --data @/var/www/html/tojson/json/addmappinglb.json --trace /var/www/html/tojson/logs/tmp';
-		// $_SESSION['log'] = shell_exec($script);
-		// if(empty($_SESSION['log'])) $_SESSION['log'] = 'Mapeamento adicionado com sucesso';
-		echo "chegou aqui";
+		$serverODL='10.132.12.138:8181';
+		MappingControle::make_addmapping_lb();
+		$script='curl -u "admin":"admin" -H "Content-type: application/json" -X POST http://'.$serverODL.'/restconf/operations/odl-mappingservice:add-mapping --data @/var/www/html/tojson/json/addmappinglb.json --trace /var/www/html/tojson/logs/tmp';
+		$_SESSION['log'] = shell_exec($script);
+		if(empty($_SESSION['log'])) $_SESSION['log'] = 'Mapeamento adicionado com sucesso';
 	}
 	public function removemapping(){
 		if(!isset($_SESSION['serverODL']) || empty($_SESSION['serverODL'])) header('location: ../../index.php');
